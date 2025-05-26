05fcc75d754b644857550c541cd981f57375e6bc
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-05-27 00:15:01 +0530

    modified

--- src/app/api/contest/route.ts
+++ src/app/api/contest/route.ts
@@ -1,78 +1,21 @@
-import axios from "axios";
-import {
-    CODECHEF_CONTESTS_ENDPOINT,
-    CODEFORCES_CONTESTS_ENDPOINT,
-    GEEKSFORGEEKS_CONTESTS_ENDPOINT,
-    LEETCODE_CONTESTS_QUERY,
-    LEETCODE_ENDPOINT,
-} from "lib/app/utils/apiConfig";
-import { cfUpcomingContests, lcUpcomingContests } from "lib/app/utils/UpcomingContest";
-import {
-    simplifiedCcContests,
-    simplifiedCfContests,
-    simplifiedGfgContests,
-    simplifiedLcContests,
-} from "lib/app/utils/simplifiedContests";
 
-export async function GET() {
-    const codeforcesResponse = await axios.get(CODEFORCES_CONTESTS_ENDPOINT, {
-        headers: {
-            "content-type": "application/json",
-        },
-    });
-
-    const cfContests = simplifiedCfContests(cfUpcomingContests(codeforcesResponse.data.result))
+import { NextRequest, NextResponse } from "next/server";
+import { fetchContestData } from "./fetchContestData";
 
 
-    const codechefResponse = await axios.get(CODECHEF_CONTESTS_ENDPOINT, {
-        headers: {
-            "content-type": "application/json",
-        },
-    });
+//filter based on source, date  sort based on start time
+//http://localhost:3000/api/contest?source=All&date=All&sort=ac
 
 
 
-    const ccContests = simplifiedCcContests(codechefResponse.data.future_contests)
 
-
-
-    const geeksforgeeksResponse = await axios.get(
-        GEEKSFORGEEKS_CONTESTS_ENDPOINT,
-        {
-            headers: {
-                "content-type": "application/json",
-            },
-        }
-    );
-
-    const gfgContests = simplifiedGfgContests(
-        geeksforgeeksResponse.data.results.upcoming
-    ); // array of objects
-
-    const leetcodeResponse = await axios.post(
-        LEETCODE_ENDPOINT,
-        {
-            query: LEETCODE_CONTESTS_QUERY,
-        },
-        {
-            headers: {
-                "content-type": "application/json",
-            },
-        }
-    );
-
-    const lcContests = simplifiedLcContests(
-        lcUpcomingContests(leetcodeResponse.data.data.allContests)
-    );// array of objects
-
-    const upcomingContests = [
-        ...cfContests,
-        ...ccContests,
-        ...gfgContests,
-        ...lcContests
-    ]
+export async function GET(req: NextRequest) {
     
-    return Response.json({
+    const upcomingContests =await fetchContestData();
+    upcomingContests.sort((a,b) => a.startTime-b.startTime)
+
+
+    return NextResponse.json({
         upcomingContests,
     });
 }
