93416979e9ffd2f38b60b586466b9c12f86178a4
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-06-11 19:05:50 +0530

    commit

---
+++ src/app/components/pill.tsx
@@ -0,0 +1,10 @@
+export default function Pill() {
+    return (
+        <div className="flex flex-row justify-between max-w-5xl border-2 border-blue-500">
+            <div>Contest name</div>
+
+            <div>start</div>
+            <div>start</div>
+        </div>
+    );
+}
---
+++ src/app/test/page.tsx
@@ -0,0 +1,9 @@
+import Pill from "../components/pill";
+
+export default function Test() {
+    return (
+        <div className="flex justify-center items-center h-screen container mx-auto w-5xl border-2 border-red-500">
+            <Pill/>
+        </div>
+    )
+}
